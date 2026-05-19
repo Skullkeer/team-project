ed650eb33200b3f36fa0358205d920201dfb5d26
Elizabeth Woodward <9964839@schoolsnet.act.edu.au>
2026-05-19 15:59:15 +1000

    here

--- The bot main : 3.2 /random_bot.py
+++ The bot main : 3.2 /random_bot.py
@@ -3,11 +3,14 @@ class Random_bot:
     self.input = input
 
   personality = {
-        "hello": "No I don't like that word",
-        "math": "no english ha take that"
-         }
-  
-  def respond(input):
+      "hello": "No I don't like that word",
+      "math": "no english ha take that"
+       }
+    
 
-    if input == personality:
-      print("hello")
+  def respond(self):
+      return personality.get(self.input, "Unknown Status")
+
+
+c = Random_bot("hello")
+c.respond()
