4793af45f5d83da0575a195120b5d2b8d7c934a9
willowywispp <willow.woods142@gmail.com>
2026-05-20 19:01:16 +1000

    started flask work

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def serve_html():
+    result = None
+
+    if request.method == "POST":
+        value = request.form["user_input"]
+        result = f"you typed: {value}"
+
+    return render_template("template.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+#
